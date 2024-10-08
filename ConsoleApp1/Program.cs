d49416152dacb65804a6f3a91d4dd26a06859bd9
Kab-35-16 <Kab-35-16@KAB-32-13->
2024-10-08 14:45:49 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////длинна 50 ширина 20 
+            //Console.Title = "Работа с консолью";
+            //Console.WindowHeight = 25;
+            //Console.WindowWidth = 50;
+
+            //Console.WriteLine("WindowWidth : " + Console.WindowWidth);
+            //Console.WriteLine("WindowHeight: " + Console.WindowHeight);
+            //Console.ReadKey();
+
+            //Console.WriteLine("asdasd");
+            //Console.BackgroundColor = ConsoleColor.White;
+            //Console.ForegroundColor = ConsoleColor.Red;
+            //Console.WriteLine("awsd");
+            //Console.ResetColor();
+            //Console.WriteLine("ds");
+            //Console.ReadKey();
+
+            //Console.CursorVisible = false;
+            //     Console.ReadKey();
+            //if (Console.CapsLock == true)
+            //{
+            //    Console.WriteLine("The Caps Lock key is ON.");
+            //}
+
+            //else
+            //{ 
+            //    Console.WriteLine("The Caps Lock key is OFF."); 
+            //}
+            //Console.ReadKey();
+
+            //Console.WriteLine("Введите ваше имя: ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"Здравствуйте {name}");
+
+            //Console.Title = name;
+            //Console.WindowHeight = 50;
+            //Console.WindowWidth = 50;
+            //if (Console.CapsLock == true)
+            //{
+            //    Console.WriteLine("CapsLock ON");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("CapsLock off");
+            //}
+            //if (Console.NumberLock == true)
+            //{
+            //    Console.WriteLine("Numlock on");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Numlock off");
+            //}
+
+            //Console.WindowWidth = 40;
+            //Console.WindowHeight = 40;
+            //Console.WriteLine("Символ = ");
+            //string simbol = Console.ReadLine();
+            //Console.WriteLine($"ваш символ = {simbol}");
+            // Console.ReadKey();
+            //Console.Clear();
+            //Console.BackgroundColor = ConsoleColor.DarkBlue;
+            //Console.WriteLine($"Символ = {simbol}");
+            //Console.ResetColor();
+            //Console.ForegroundColor = ConsoleColor.Green;
+            //Console.BackgroundColor = ConsoleColor.Red;
+            //Console.WriteLine($"       **");
+            //Console.WriteLine($"     **  **");
+            //Console.WriteLine($"    **    **");
+            //Console.WriteLine($"   **      **");
+            //Console.WriteLine($"   **********");
+            
+            //Console.ReadKey();
+
+            
+              
+            
+
+
+        }
+    }
+}
